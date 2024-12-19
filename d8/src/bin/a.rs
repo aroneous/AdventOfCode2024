772558f27867bfee29bd2d2e0af12251857163b5
Aron Nopanen <aron.nopanen@gmail.com>
2024-12-18 19:17:47 -0800

    Day 8

---
+++ d8/src/bin/a.rs
@@ -0,0 +1,53 @@
+use std::{collections::{HashMap, HashSet}, io};
+
+fn main() {
+  let mut a: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
+
+  let mut buffer = String::new();
+  let mut y = 0;
+  let mut height = 0;
+  let mut width: i32 = 0;
+  while io::stdin().read_line(&mut buffer).is_ok() {
+    if buffer.trim().is_empty() {
+      break;
+    }
+
+    height += 1;
+    width = buffer.trim().len() as i32;
+
+    for (x, cell) in buffer.trim().chars().enumerate() {
+      if cell != '.' {
+        a.entry(cell).and_modify(|e| e.push((x as i32, y as i32)))
+        .or_insert(vec![(x as i32, y as i32)]);
+      }
+    }
+
+    buffer.clear();
+    y += 1;
+  }
+
+  // println!("{:?}", a);
+
+  let mut ans = HashSet::new();
+  for ants in a.values() {
+    for (ione, (x1, y1)) in ants.iter().enumerate() {
+      for (x2, y2) in ants[(ione + 1)..].iter() {
+        let dx = x2 - x1;
+        let dy = y2 - y1;
+        let anx1 = x1 - dx;
+        let any1 = y1 - dy;
+        if anx1 >= 0 && anx1 < width && any1 >= 0 && any1 < height {
+          ans.insert((anx1, any1));
+        }
+
+        let anx2 = x2 + dx;
+        let any2 = y2 + dy;
+        if anx2 >= 0 && anx2 < width && any2 >= 0 && any2 < height {
+          ans.insert((anx2, any2));
+        }
+      }
+    }
+  }
+
+  println!("{}", ans.len());
+}
